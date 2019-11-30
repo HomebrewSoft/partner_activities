0a94b0b793b3a014fd0c7656e75adc60e4327a00
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-30 13:51:43 -0600

    [IMP] just for companies

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Partner Activities',
-    'version': '13.0.1.1.0',
+    'version': '13.0.1.2.0',
     'author': 'HomebrewSoft',
     'website': 'https://github.com/HomebrewSoft/partner_activities',
     'category': 'Operations',
